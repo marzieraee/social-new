9ad700cd999441008b7aa48e82b03a67c15315fe
marzieraee <marzyra1997@gmail.com>
2022-08-15 15:11:33 +0430

    logout2345

--- polls/views.py
+++ polls/views.py
@@ -189,10 +189,9 @@ class MyProfile(RetrieveUpdateAPIView):
     
 
 class CookielogoutView(TokenRefreshView):
-    def finalize_response(self, request, response, *args, **kwargs):
+    def response(self, request, response, *args, **kwargs):
         if response.data.get('refresh'):
             cookie_max_age = 3600 * 24 * 14 # 14 days
-            response.set_cookie('', response.data['refresh'], max_age=cookie_max_age, httponly=True  ,samesite='None',secure=True)
-            del response.data['refresh']
-        return super().finalize_response(request, response, *args, **kwargs)
+            response.set_cookie('', max_age=cookie_max_age, httponly=True  ,samesite='None',secure=True)
+        return response
     serializer_class = CookieTokenRefreshSerializer
