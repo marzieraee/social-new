3bbd65d807d3f36662b2359f7b265fbed09feac6
marzieraee <marzyra1997@gmail.com>
2022-08-27 10:10:55 +0430

    added look feildaaaxas

--- login/views.py
+++ login/views.py
@@ -47,76 +47,9 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
         # build your response and set cookie
         if access is not None:
-            response = Response({"access": access, "refresh": refresh}, status=200)
+            response = Response({"access": access}, status=200)
             response.set_cookie('refresh', refresh, httponly=True,samesite='None')
             return response
-        
 
         return Response({"Error": "Something went wrong"}, status=400)
 
-# @api_view(['POST'])
-# @permission_classes([AllowAny])
-# @ensure_csrf_cookie
-# def login_view(request):
-#     User = get_user_model()
-#     username = request.data.get('username')
-#     password = request.data.get('password')
-#     response = Response()
-#     if (username is None) or (password is None):
-#         raise exceptions.AuthenticationFailed(
-#             'username and password required')
-
-#     user = User.objects.filter(username=username).first()
-#     if(user is None):
-#         raise exceptions.AuthenticationFailed('user not found')
-#     if (not user.check_password(password)):
-#         raise exceptions.AuthenticationFailed('wrong password')
-
-#     serialized_user = UserSerializer(user).data
-
-#     access_token = generate_access_token(user)
-#     refresh_token = generate_refresh_token(user)
-
-#     response.set_cookie(key='refreshtoken', value=refresh_token, httponly=True ,samesite='None',secure=True)
-#     response.data = {
-#         'access_token': access_token,
-#         # 'user': serialized_user,
-#     }
-
-#     return response
-
-
-
-
-# @api_view(['POST'])
-# @permission_classes([AllowAny])
-# @ensure_csrf_cookie
-# def refresh_token_view(request):
-#     '''
-#     To obtain a new access_token this view expects 2 important things:
-#         1. a cookie that contains a valid refresh_token
-#         2. a header 'X-CSRFTOKEN' with a valid csrf token, client app can get it from cookies "csrftoken"
-#     '''
-#     User = get_user_model()
-#     refresh_token = request.COOKIES.get('refreshtoken')
-#     if refresh_token is None:
-#         raise exceptions.AuthenticationFailed(
-#             'Authentication credentials were not provided.')
-#     try:
-#         payload =jwt.decode(
-#             refresh_token, settings.SECRET_KEY, algorithms=['HS256'])
-#         print(jwt)
-#     except jwt.ExpiredSignatureError:
-#         raise exceptions.AuthenticationFailed(
-#             'expired refresh token, please login again.')
-
-#     user = User.objects.filter(id=payload.get('user_id')).first()
-#     if user is None:
-#         raise exceptions.AuthenticationFailed('User not found')
-
-#     if not user.is_active:
-#         raise exceptions.AuthenticationFailed('user is inactive')
-
-
-#     access_token = generate_access_token(user)
-#     return Response({'access_token': access_token})
