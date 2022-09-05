48e6ce29e06ebc6c4e66a83d1de1bc239697f05b
marzieraee <marzyra1997@gmail.com>
2022-09-05 15:54:26 +0430

    alot change123aaj

--- mysite/settings.py
+++ mysite/settings.py
@@ -256,10 +256,10 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'formydjango@gmail.com'
-EMAIL_HOST_PASSWORD = 'jrsnfimijfoxcfdi'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'formydjango@gmail.com'
+# EMAIL_HOST_PASSWORD = 'jrsnfimijfoxcfdi'
