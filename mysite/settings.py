61c936261fd311a93821f8e2bcd9fb1825ee2035
marzieraee <marzyra1997@gmail.com>
2022-09-06 11:58:41 +0430

    procfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -256,10 +256,17 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+# EMAIL_PORT = 45848
+# EMAIL_HOST_USER = 'formydjango@gmail.com'
+# EMAIL_HOST_PASSWORD = 'jrsnfimijfoxcfdi'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.cloudmta.net'
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_PORT = 45848
-EMAIL_HOST_USER = 'formydjango@gmail.com'
-EMAIL_HOST_PASSWORD = 'jrsnfimijfoxcfdi'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'e7d6dea731b2f948'
+EMAIL_HOST_PASSWORD = '1KkCQUAMLpQjaYxVf6oF75Qp'
